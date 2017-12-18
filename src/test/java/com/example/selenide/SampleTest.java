42a98e2a8be6f8ac4af25fa6438144bc7768e000
SHIMANE, Yoshikazu <shima4@gmail.com>
2017-12-18 23:51:06 +0900

    録画機能追加

--- src/test/java/com/example/selenide/SampleTest.java
+++ src/test/java/com/example/selenide/SampleTest.java
@@ -6,23 +6,92 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.example.selenide.page.CheckInfoPage;
 import com.example.selenide.page.FinalConfirmPage;
 import com.example.selenide.page.InputPage;
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.monte.media.Format;
+import org.monte.media.math.Rational;
+
+import java.awt.*;
+import java.io.File;
 
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+import static org.monte.media.FormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
 
 /**
  * Unit test for simple App.
  */
 public class SampleTest {
 
+    public void startRecording(String name) throws Exception {
+        File file = new File("record");
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        Rectangle captureSize = new Rectangle(0,0, width, height);
+
+        GraphicsConfiguration gc = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration();
+
+        this.screenRecorder = new ScreenRecorder(gc, captureSize,
+                new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey,  MIME_AVI),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        DepthKey, 24, FrameRateKey, Rational.valueOf(15),
+                        QualityKey, 1.0f,
+                        KeyFrameIntervalKey, 15 * 60),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black",
+                        FrameRateKey, Rational.valueOf(30)),
+                null, file, name);
+        this.screenRecorder.start();
+    }
+
+    public void stopRecording() throws Exception {
+        this.screenRecorder.stop();
+    }
+
+    private ScreenRecorder screenRecorder;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+
+        @Override
+        protected void succeeded(Description d) {
+            System.out.println("I am succeeded() method. name -> " + d.getMethodName());
+            screenRecorder.getCreatedMovieFiles().stream().filter(a -> a.toPath().toString().contains(testName.getMethodName())).forEach(a -> a.deleteOnExit());
+        }
+
+        @Override
+        protected void failed(Throwable th, Description d) {
+            System.out.println("I am failed() method. name -> " + d.getMethodName());
+            System.out.println(th.toString());
+        }
+    };
+
+
     @Before
-    public void before(){
+    public void before() throws Exception {
         Configuration.browser = WebDriverRunner.GECKO;
+        startRecording(testName.getMethodName());
+        
+    }
+
+    public void after() throws Exception {
+        stopRecording();
     }
 
     @Test
